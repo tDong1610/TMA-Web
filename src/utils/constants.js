584cfb0b94043a8735594cd0ca2745d4e21a2454
tDong1610 <165650614+tDong1610@users.noreply.github.com>
2025-06-01 19:02:37 +0700

    Update constants.js

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -10,7 +10,7 @@ if (process.env.BUILD_MODE === 'dev') {
 // Môi trường Production sẽ cần api endpoint chuẩn của các bạn
 if (process.env.BUILD_MODE === 'production') {
   // Lưu ý: Đây là domain ví dụ sau khi Deploy Production (xem video 75 và video 76 để hiểu rõ kiến thức phần này, còn hiện tại mình đã xóa domain này rồi, đừng cố truy cập làm gì =))
-  apiRoot = 'https://tma-api.onrender.com'
+  apiRoot = 'https://tma-web-qldapmcc03.vercel.app'
 }
 // console.log('🚀 ~ file: constants.js:7 ~ apiRoot:', apiRoot)
 export const API_ROOT = apiRoot
